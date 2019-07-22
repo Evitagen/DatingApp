0aa1152e46af0918648f5e3b16e80e6ecf6c3c26
Tim Schofield <timschofield@Tims-MBP.lan>
2019-07-22 22:57:16 +0100

    end of section 5

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -55,6 +59,19 @@ namespace DatingApp.API
             }
             else
             {
+                app.UseExceptionHandler(builder => {                                                    //
+                    builder.Run(async context => {                                                      //  This is instead of try catch block for catching exceptions 
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;          //  globally
+                                                                                                        //
+                        var error = context.Features.Get<IExceptionHandlerFeature>();                   //
+                        if (error != null)                                                              //
+                        {                                                                               //
+                            context.Response.AddApplicationError(error.Error.Message);                  //
+                            await context.Response.WriteAsync(error.Error.Message);                     //
+                        }                                                                               //
+                    });                                                                                 //
+                });                                                                                     //
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
