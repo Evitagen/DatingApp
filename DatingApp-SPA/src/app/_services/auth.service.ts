bbe9668b1d5f52d93610e52775850af7d7a688d0
Tim Schofield <timschofield@Tims-MBP.lan>
2019-07-24 12:31:37 +0100

    login bar working correctly

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { createWiresService } from 'selenium-webdriver/firefox';
 
 
 @Injectable({
@@ -8,6 +11,8 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
   baseUrl = 'http://Localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -17,6 +22,8 @@ login(model: any) {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
     })
   );
@@ -24,6 +31,11 @@ login(model: any) {
 
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
